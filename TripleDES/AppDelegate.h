f2e5c79d3e2cc710cb5c738477dc779cab4636b0
xujiebing <xujiebing@bwton.com>
2018-05-11 14:55:59 +0800

    Initial Commit

---
+++ TripleDES/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TripleDES
+//
+//  Created by 徐结兵 on 2018/5/11.
+//  Copyright © 2018年 八维通. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TripleDES/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TripleDES
+//
+//  Created by 徐结兵 on 2018/5/11.
+//  Copyright © 2018年 八维通. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
